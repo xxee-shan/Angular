89061a5085980072cc7e606626e80d9c38571fc8
xxee-shan <zishan.extcengr@gmail.com>
2021-07-24 00:11:19 +0530

    assignment-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
  
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
+import { AlertsComponent } from './components/alerts/alerts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    AlertsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: "<p>warning- you entered warning alert component!</p>",
+  styles: [`
+  p{ 
+    color: red
+  }
+  `]
+})
+export class WarningAlertComponent {
+
+}
